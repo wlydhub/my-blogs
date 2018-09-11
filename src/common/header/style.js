18deb11dd26f2b69e07ae175e15ee535c62305b5
王力宇 <915153147@qq.com>
2018-09-11 15:57:46 +0800

    【header】完善头部导航的基本样式

--- src/common/header/style.js
+++ src/common/header/style.js
@@ -5,7 +5,7 @@ export const HeaderWrapper = styled.div`
   height: 56px;
   border-bottom: 1px solid red;
   position: relative;
-`
+`;
 export const Logo = styled.a`
   height: 58px;
   position: absolute;
@@ -17,4 +17,64 @@ export const Logo = styled.a`
   background: url(${ logoPic });
   background-size: contain;
   background-repeat:no-repeat;
-`
+`;
+export const Nav = styled.div`
+  width:960px;
+  height:100%;
+  padding-right: 70px;
+  box-sizing: border-box;
+  margin: 0 auto;
+`;
+export const NavItem = styled.div`
+  line-height:56px;
+  padding: 0 15px;
+  font-size:17px;
+  color:#333;
+  &.left {
+    float:left;
+  }
+  &.right {
+    float:right;
+    color:#969696;
+  }
+  &.active {
+    color:red;
+  }
+`;
+export const NavSearch = styled.input.attrs({
+    placeholder:'搜索'
+  })`
+  width: 160px;
+  height:38px;
+  margin-top: 8px;
+  margin-left: 20px;
+  box-sizing: border-box;
+  border: none;
+  outline: none;
+  border-radius: 19px;
+  background: #eee;
+  font-size:14px;
+`;
+export const Addition = styled.div`
+  position: absolute;
+  right:0;
+  top:0;
+  height:56px;
+`;Button
+export const Button = styled.div`
+  float:right;
+  margin-top:9px;
+  margin-right:20px;
+  padding:0 20px;
+  line-height:38px;
+  border-radius:19px;
+  border: 1px solid red;
+  font-size:14px;
+  &.reg {
+    color: #ec6149;
+  }
+  &.writting {
+    color:#fff;
+    background:#ec6149;
+  }
+`;
