190e06f6e9d8acb895499e24bc7f41370bd857f6
王力宇 <915153147@qq.com>
2018-09-12 17:48:29 +0800

    使用immutable确保state不会被改变

--- src/common/header/store/reducer.js
+++ src/common/header/store/reducer.js
@@ -1,18 +1,15 @@
 import * as constants from './constants.js';
-const defaultState = {
-    focused: false,
-};
+import { fromJS } from 'immutable';
+const defaultState = fromJS({
+  focused: false,
+});
   
 export default (state = defaultState, action) => {
   if (action.type === constants.SEARCH_FOCUS) {
-    return {
-      focused: true,
-    }
+    return state.set('focused', true);
   }
   if (action.type === constants.SEARCH_BLUR) {
-    return {
-      focused: false,
-    }
+    return state.set('focused', false);
   }
   return state;
 };
