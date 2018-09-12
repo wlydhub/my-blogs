37d55bc2673853dcbc95ef4b76baf0cfa0822538
王力宇 <915153147@qq.com>
2018-09-12 16:46:17 +0800

    引入redux,react-redux，对数据进行管理

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,72 +1,72 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { connect } from 'react-redux';
 import { CSSTransition } from 'react-transition-group';
 import { HeaderWrapper, Logo, Nav, NavItem, SerchWrapper, NavSearch, Addition, Button } from './style';
 
-class Header extends Component {
-
-  constructor(props){
-    super(props);
-    this.state = {
-      focused: false,
-    }
-    this.handleInputFocus = this.handleInputFocus.bind(this);
-    this.handleInputBlur = this.handleInputBlur.bind(this);
-  }
-
-  render(){
-    return(
-      <HeaderWrapper>
-        <Logo href='./' />
-        <Nav>
-          <NavItem className='left active'>首页</NavItem>
-          <NavItem className='left'>下载app</NavItem>
-          <NavItem className='right'>登录</NavItem>
-          <NavItem className='right'>
-            <i className='iconfont'>&#xe636;</i>
-          </NavItem>
-          <SerchWrapper>
-            <CSSTransition
-              in = { this.state.focused }
-              timeout = { 300 }
-              classNames = "slide"
+const Header = (props) => {
+  return(
+    <HeaderWrapper>
+      <Logo href='./' />
+      <Nav>
+        <NavItem className='left active'>首页</NavItem>
+        <NavItem className='left'>下载app</NavItem>
+        <NavItem className='right'>登录</NavItem>
+        <NavItem className='right'>
+          <i className='iconfont'>&#xe636;</i>
+        </NavItem>
+        <SerchWrapper>
+          <CSSTransition
+            in = { props.focused }
+            timeout = { 300 }
+            classNames = "slide"
+          >
+            <NavSearch
+              className = {props.focused ? 'focused': ''}
+              onFocus = {props.handleInputFocus}
+              onBlur = {props.handleInputBlur}
             >
-              <NavSearch
-                className = {this.state.focused ? 'focused': ''}
-                onFocus = {this.handleInputFocus}
-                onBlur = {this.handleInputBlur}
-              >
-              </NavSearch>
-            </CSSTransition>
-            <i 
-              className = {this.state.focused ? 'focused iconfont': 'iconfont'}
-            >
+            </NavSearch>
+          </CSSTransition>
+          <i 
+            className = {props.focused ? 'focused iconfont': 'iconfont'}
+          >
             &#xe637;
-            </i>
-          </SerchWrapper>
-        </Nav>
-        <Addition>
-          <Button className='writting'>
-            <i className='iconfont'>&#xe615;</i>
-            写文章
-          </Button>
-          <Button className='reg'>注册</Button>  
-        </Addition>
-      </HeaderWrapper>
-    )
-  }
+          </i>
+        </SerchWrapper>
+      </Nav>
 
-  handleInputFocus(){
-    this.setState ({
-      focused: true,
-    })
-  }
-
-  handleInputBlur(){
-    this.setState ({
-      focused: false,
-    })
-  }
-
+      <Addition>
+        <Button className='writting'>
+          <i className='iconfont'>&#xe615;</i>
+          写文章
+        </Button>
+        <Button className='reg'>注册</Button>  
+      </Addition>
+    </HeaderWrapper>
+  )
 }
 
-export default Header;
+const mapStateToProps = (state) => {
+  return {
+    focused: state.focused,
+  }
+}
+
+const mapDispathToProps = (dispatch) => {
+  return {
+    handleInputFocus(){
+      const action = {
+        type: 'search_focus'
+      };
+      dispatch(action);
+    },
+    handleInputBlur(){
+      const action = {
+        type: 'search_blur'
+      };
+      dispatch(action);
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispathToProps)(Header);
--- src/common/header/style.js
+++ src/common/header/style.js
@@ -44,18 +44,6 @@ export const NavItem = styled.div`
 export const SerchWrapper = styled.div`
   position: relative;
   float: left;
-  .slide-enter {
-    transition: all .3s ease-out;
-  }
-  .slide-enter-active {
-    width: 240px;
-  }
-  .slide-exit {
-    transition: all .3s ease-out;
-  }
-  .slide-exit-active {
-    width: 160;
-  }
   .iconfont {
     position: absolute;
     right: 5px;
@@ -90,6 +78,18 @@ export const NavSearch = styled.input.attrs({
   }
   &.focused {
     width: 240px;
+  }
+  &.slide-enter {
+    transition: all .3s ease-out;
+  }
+  &.slide-enter-active {
+    width: 240px;
+  }
+  &.slide-exit {
+    transition: all .3s ease-out;
+  }
+  &.slide-exit-active {
+    width: 160;
   }
 `;
 export const Addition = styled.div`
