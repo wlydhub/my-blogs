d1956f6e702c79975e7e479cb7d708b2f00237a8
王力宇 <915153147@qq.com>
2018-10-10 18:32:54 +0800

    添加文章详情页，初步完成详情页

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { DetailWrapper, Header, Content } from './style';
 
 class Detail extends Component {
   render() {
     return (
-      <div>detail~</div>
+      <DetailWrapper>
+        <Header>{ this.props.title }</Header>
+        <Content dangerouslySetInnerHTML = {{__html: this.props.content}} />
+      </DetailWrapper>
     )
   }
 }
 
-export default Detail;
+const mapState = (state) => ({
+  title: state.getIn(['detail', 'title']),
+  content: state.getIn(['detail', 'content']),
+})
+
+export default connect(mapState, null)(Detail);
