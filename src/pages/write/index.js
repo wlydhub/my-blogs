938a4cad77c3fd958eb85c12ab7bb1eba87afbbb
王力宇 <915153147@qq.com>
2018-10-13 11:24:02 +0800

    新增react-quill模块，初始化文章编辑页面

--- src/pages/write/index.js
+++ src/pages/write/index.js
@@ -1,16 +1,27 @@
 import React, { PureComponent } from 'react';
 import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
 class Write extends PureComponent {
+
+  constructor(props) {
+    super(props)
+    this.state = { text: '' } // You can also pass a Quill Delta here
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(value) {
+    this.setState({ text: value })
+  }
 
   render() {
     const { loginStatus } = this.props;
     if(loginStatus){
       return (
-        <div>
-          this is write page
-        </div>
+        <ReactQuill value={this.state.text}
+                    onChange={this.handleChange} />
       )
     } else {
       return (
@@ -22,6 +33,9 @@ class Write extends PureComponent {
 
 const mapState = (state) => ({
   loginStatus: state.getIn(['login', 'login'])
-})
+});
+
+
+
 
 export default connect(mapState, null)(Write);
