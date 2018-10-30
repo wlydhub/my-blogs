5811b680b6069e86d7b1a626d2765cb19021f908
王力宇 <915153147@qq.com>
2018-10-30 17:58:36 +0800

    写文章部分（引入quill成功）

--- src/pages/write/index.js
+++ src/pages/write/index.js
@@ -1,43 +1,92 @@
-import React, { PureComponent } from 'react';
-import { Redirect } from 'react-router-dom';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
+import Quill from "quill";
+import 'quill/dist/quill.snow.css';
+import { WriteWrapper, ToolbarWrapper, EditorWrapper, Butten } from './style';
+class Write extends Component {
 
-class Write extends PureComponent {
-
-  constructor(props) {
-    super(props)
-    this.state = { text: '' } // You can also pass a Quill Delta here
-    this.handleChange = this.handleChange.bind(this)
-  }
-
-  handleChange(value) {
-    this.setState({ text: value })
-  }
+  handleChange = () => {
+    // change 事件将HTML字符串更新到state里面，
+    this.setState({
+      value: this.editor.root.innerHTML,
+      mediaVisbile: false
+    });
+  };
 
   render() {
-    const { loginStatus } = this.props;
-    if(loginStatus){
-      return (
-        <ReactQuill 
-          value={this.state.text}
-          onChange={this.handleChange} 
-        />
-      )
-    } else {
-      return (
-        <Redirect to = '/login' />
-      )
-    }
+    return (
+      <WriteWrapper>
+        <ToolbarWrapper id="toolbar">
+          <select className="ql-header" defaultValue="small">
+            <option value="1">大标题</option>
+            <option value="2">副标题</option>
+            <option value="3">小标题</option>
+          </select>
+          <select className="ql-size" defaultValue="small">
+            <option value="small">小字体</option>
+            <option value="large">大字体</option>
+            <option value="huge">巨大字体</option>
+          </select>
+          <select className="ql-color" defaultValue="black">
+            <option value="black" />
+            <option value="red" />
+            <option value="yellow" />
+            <option value="blue" />
+            <option value="green" />
+          </select>
+          <select className="ql-background" defaultValue="">
+            <option value="red" />
+            <option value="yellow" />
+            <option value="blue" />
+            <option value="green" />
+          </select>
+          <button className="ql-list" value="ordered"></button>
+          <button className="ql-list" value="bullet"></button>
+          <button className="ql-script" value="sub"></button>
+          <button className="ql-script" value="super"></button>
+          <button className="ql-clean"></button>
+          <button className="ql-image"></button>
+          <button className="ql-video"></button>
+		      <Butten
+            onClick={() =>
+              console.log("这里可以通过读取state来拿到所有的html字符串:", this.state)
+            }
+          >
+            保存
+          </Butten>
+        </ToolbarWrapper>
+        <EditorWrapper id="editor" />
+      </WriteWrapper>
+    )
+  };
+
+  componentDidMount() {
+    // 配置项，在Quill 官网上有详细说明
+    const options = {
+      theme: "snow",
+      modules: { // 自定义 toolbar 填写这个属性， 值写上 div 的 id
+        toolbar: "#toolbar",
+      }
+    };
+    // 实例化 Quill 并且储存在实例原型上
+    this.editor = new Quill("#editor", options);
+    // 实现输入受控，从state中读取html字符串写入编辑器中
+    const value= '';
+    // 判断value中是否有值，如果有那么就写入编辑器中
+    if (value) this.editor.clipboard.dangerouslyPasteHTML(value);
+    // 设置事件，change事件，
+    this.editor.on("text-change", this.handleChange);
   }
+
 }
 
 const mapState = (state) => ({
   loginStatus: state.getIn(['login', 'login'])
 });
 
+const mapDispatch = (dispatch) => ({
 
+});
 
+export default connect(mapState, mapDispatch)(Write);
 
-export default connect(mapState, null)(Write);
---
+++ src/pages/write/style.js
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+
+export const WriteWrapper = styled.div`
+  position: absolute;
+  top: 56px;
+  left: 0px;
+  right: 0px;
+  bottom: 0px;
+`;
+
+export const ToolbarWrapper = styled.div`
+  display: "flex";
+  alignItems: "center";
+  .update: {
+    width: 60px;
+    background: blue;
+    float: left;
+  };
+`;
+
+export const Butten = styled.div`
+  position: absolute;
+  right: 10px;
+  width: 80px;
+  height: 30px;
+  line-height: 30px;
+  color: #fff;
+  background: #3194d0;
+  border-radius: 15px;
+  text-align: center;
+`;
+
+export const EditorWrapper = styled.div`
+  height: 100%;
+`;
+
